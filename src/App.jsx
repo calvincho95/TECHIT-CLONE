080aa93bdd627ba612830643b699a98691b56e4c
calvincho95 <calvincho95@naver.com>
2024-05-22 15:17:59 +0900

    TechitCloneFin

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import StripBanner from "./components/StripBanner";
 import OnboardingSection from "./components/OnboardingSection";
 import TrendSection from "./components/TrendSection";
 import EducationSection from "./components/EducationSection";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [isViewed, setIsViewed] = useState(false);
@@ -31,6 +32,10 @@ const App = () => {
       <OnboardingSection />
       <TrendSection />
       <EducationSection />
+      <Footer />
+      <button className="w-[216px] fixed bottom-12 right-4">
+        <img src="/images/ask.png" alt="문의하기" />
+      </button>
     </div>
   );
 };
